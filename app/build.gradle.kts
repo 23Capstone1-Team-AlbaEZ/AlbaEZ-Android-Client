e81332942cf210137d137eb14ac4eeb18641aec8
TaeHyun Kim <devupkim@gmail.com>
2023-11-14 17:07:21 +0900

    SDK Version Update to 34
    종속성 버전 이슈로 인해 34로 업그레이드 후 리빌드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.albaez.albaez_client"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.albaez.albaez_client"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
