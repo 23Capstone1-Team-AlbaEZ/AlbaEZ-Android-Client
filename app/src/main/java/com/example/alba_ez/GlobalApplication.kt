73d3562592f69aa44dd839a6b9158bc71fbf06e6
youyo <youyou1298@naver.com>
2023-11-23 20:07:49 +0900

    .

--- app/src/main/java/com/example/alba_ez/GlobalApplication.kt
+++ app/src/main/java/com/example/alba_ez/GlobalApplication.kt
@@ -2,9 +2,14 @@ package com.example.alba_ez
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import android.util.Log
+import com.kakao.sdk.common.util.Utility
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, "d4be87432c543840305997a2e113d219")
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
     }
 }
